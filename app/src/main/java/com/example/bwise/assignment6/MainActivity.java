568db7002383d8e2a479ecd758a22dd66d4a71db
alwaysbwise <33883062+alwaysbwise@users.noreply.github.com>
2018-02-19 15:45:48 -0800

    adds dark theme to custom tip calculator app

--- app/src/main/java/com/example/bwise/assignment6/MainActivity.java
+++ app/src/main/java/com/example/bwise/assignment6/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bwise.assignment6;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -9,13 +11,14 @@ import android.widget.Toast;
 
 import java.text.NumberFormat;
 
-public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener{
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener, Button.OnClickListener{
 
     // create instance variables
     private EditText editTextEntry;
     private TextView textViewValue;
     private TextView textViewPercent;
     private SeekBar seekBar;
+    private Button button;
 
     // define instance variables that should be saved
     private String billAmountString = "";
@@ -31,9 +34,11 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
         textViewValue = (TextView) findViewById(R.id.textViewValue);
         textViewPercent = (TextView) findViewById(R.id.textViewPercent);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+        button = (Button) findViewById(R.id.button);
 
         //set listener
         seekBar.setOnSeekBarChangeListener(this);
+        button.setOnClickListener(this);
 
         textViewPercent.setText("Tip Percentage:    " + 18);
     }
@@ -54,19 +59,13 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
         // display the other results with formatting
         NumberFormat currency = NumberFormat.getCurrencyInstance();
         textViewValue.setText(currency.format(tipAmount));
-
-
-
     }
-
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
         //Toast.makeText(getApplicationContext(), "Percentage: " + progress, Toast.LENGTH_SHORT).show();
         textViewPercent.setText("Tip Percentage:    " + progress);
-
     }
-
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
 
@@ -75,6 +74,11 @@ public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeLis
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         Toast.makeText(getApplicationContext(), "Tip Percentage Set", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onClick(View view) {
         calculateAndDisplay();
 
     }
